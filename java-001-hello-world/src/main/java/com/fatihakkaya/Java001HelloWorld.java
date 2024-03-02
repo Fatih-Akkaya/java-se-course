e2eaea10662676ce8531e14b402c1c8a3a3123d4
Fatih AKKAYA <fatihakkaya@hotmail.com>
2024-03-03 01:37:02 +0300

    Hello World

---
+++ java-001-hello-world/src/main/java/com/fatihakkaya/Java001HelloWorld.java
@@ -0,0 +1,12 @@
+package com.fatihakkaya;
+
+/**
+ *
+ * @author Fatih
+ */
+public class Java001HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
